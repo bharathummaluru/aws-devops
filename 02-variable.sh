e1532d5a6c9c0b4cf06a730da89d2fa1defd32e1
Bharath Reddy T <bharathummaluru@gmail.com>
2024-08-27 22:24:57 +0530

    variables

--- 02-variable.sh
+++ 02-variable.sh
@@ -3,5 +3,5 @@
 PERSON1=$1
 PERSON2=$2
 
-echo "$PERSON1, Hello ${PERSON2} How are you?"
-echo "$PERSON2, Hello $PERSON1 I am doing good"
+echo "$PERSON1::, Hello ${PERSON2} How are you?"
+echo "$PERSON2::, Hello $PERSON1 I am doing good"
